4ea81a0758ef508e3d2468ca6b20df2d87cdea7b
alpha-knowledge <anupalla00gmail.com>
2026-02-02 18:05:18 +0530

    disable datasource for render

--- rideshare/src/main/java/com/rideshare/RideshareApplication.java
+++ rideshare/src/main/java/com/rideshare/RideshareApplication.java
@@ -2,8 +2,10 @@ package com.rideshare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class  RideshareApplication {
 
 	public static void main(String[] args) {
